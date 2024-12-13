8fe5b606cacf2f6323a58cc5596b354ac2c9d76e
Dan Lindholm <git@dan.mk>
2024-12-13 13:10:51 +0100

    add minimal pluralize function

--- src/docker_snapshot/utils.py
+++ src/docker_snapshot/utils.py
@@ -1,3 +1,6 @@
+import typing as t
+
+
 # Fuck this name
 def sizeof_fmt(num: float, suffix: str = "B") -> str:
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
@@ -5,3 +8,40 @@ def sizeof_fmt(num: float, suffix: str = "B") -> str:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, "Yi", suffix)
+
+
+@t.overload
+def pluralize(
+    word: str,
+    n: int,
+    *,
+    suffix: str,
+) -> str: ...
+
+
+@t.overload
+def pluralize(
+    word: str,
+    n: int,
+    *,
+    plural: str,
+) -> str: ...
+
+
+def pluralize(
+    word: str,
+    n: int,
+    *,
+    suffix: t.Optional[str] = None,
+    plural: t.Optional[str] = None,
+) -> str:
+    if n == 1:
+        return word
+
+    if plural is not None:
+        return plural
+
+    if suffix is not None:
+        return f"{word}{suffix}"
+
+    raise ValueError("either `suffix` or `plural` must be non-null")
